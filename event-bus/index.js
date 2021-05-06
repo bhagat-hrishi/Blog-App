5e309f7da26b1a04627627e8b3703dcb6479ec6e
Hrishi007 <hrishibhagat8642@gmail.com>
2021-05-06 12:37:59 +0530

    updated event-bus

--- event-bus/index.js
+++ event-bus/index.js
@@ -6,15 +6,33 @@ const app = express();
 app.use(bodyParser.json());
 
 
+const storeEvents = [];
+
 app.post('/events',(req,res)=>{
 
     const event = req.body;
 
-    axios.post('http://localhost:4000/events',event);
-    axios.post('http://localhost:4001/events',event);
-    axios.post('http://localhost:4002/events',event);
+    storeEvents.push(event);
+    axios.post('http://localhost:4000/events',event)
+    .catch((err) => {
+        console.log(err.message);
+    });//posts
+    axios.post('http://localhost:4001/events',event).catch((err) => {
+        console.log(err.message);
+    });;//comments
+    axios.post('http://localhost:4002/events',event).catch((err) => {
+        console.log(err.message);
+    });;//query
+    axios.post('http://localhost:4003/events',event).catch((err) => {
+        console.log(err.message);
+    });;//moderation service
 
     res.send({status:"OK"})
+})
+
+// To get all events
+app.get('/events',(req,res)=>{
+    res.send(storeEvents);
 })
 
 const PORT=4005;
